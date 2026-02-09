3ee9c1f5bde9b9f5a7bd9a43ca0ba4eeaa9cfc86
dojhoanrd-jw <dojhoanrd.jw@gmail.com>
2026-02-08 21:48:51 -0800

    doc: comment in Page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
+// This is the root page of the application. It redirects to the dashboard page.
 export default function Home() {
   redirect("/dashboard");
 }
